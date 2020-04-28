3d985bee0c501c0ba03cbc2b1ee400c0cfcc08cc
benakehurst <benakehurst@hotmail.com>
2020-04-28 12:08:54 +0100

    feat(card): add component, css and stories

--- src/Components/Molecules/Card/Card.js
+++ src/Components/Molecules/Card/Card.js
@@ -1,16 +1,125 @@
 import React, { Component } from 'react';
 import classes from './Card.module.css';
+import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Aux';
 
+import Image from '../../Atoms/Image/Image';
+import Button from '../../Atoms/Button/Button';
+
 class Card extends Component {
+  handleCardClicked = (e) => {
+    console.log('Button Clicked');
+  };
+
+  generateImage = () => {
+    return (
+      <div className={classes.ImageWrapper}>
+        <Image
+          size={this.props.size}
+          imageLink={this.props.imageLink}
+          altText={this.props.imageAltText}
+          corners={this.props.imageShape}
+        />
+      </div>
+    );
+  };
+
+  genetateText = () => {
+    return (
+      <div className={classes.TextWrapper}>
+        <p>{this.props.cardText}</p>
+      </div>
+    );
+  };
+
+  generateButton = () => {
+    return (
+      <div className={classes.ButtonWrapper}>
+        <Button
+          clicked={(e) => this.handleCardClicked(e)}
+          size={this.props.size}
+          buttonType={this.props.buttonType}
+          shadow={this.props.shadow}
+          corners={this.props.corners}
+          disabled={this.props.disabled}
+        >
+          {this.props.buttonText}
+        </Button>
+      </div>
+    );
+  };
+
   render() {
     return (
       <Aux>
-        <div className={classes.Card}></div>
+        <div className={[classes.Card, classes[this.props.size]].join(' ')}>
+          {this.props.isImage ? this.generateImage() : null}
+          {this.props.isText ? this.genetateText() : null}
+          {this.props.isButton ? this.generateButton() : null}
+        </div>
       </Aux>
     );
   }
 }
+
+Card.propTypes = {
+  isImage: PropTypes.bool,
+  /**
+   * If `true`, an image will be shown on the card
+   */
+  isText: PropTypes.bool,
+  /**
+   * If `true`, text will be shown on the card
+   */
+  isButton: PropTypes.bool,
+  /**
+   * If `true`, a button will be shown on the card
+   */
+  cardText: PropTypes.string,
+  /**
+   * Defines the text to be shown on the the card
+   */
+  size: PropTypes.string,
+  /**
+   'small' | 'medium' | 'large'
+  */
+  imageLink: PropTypes.string,
+  /**
+   A link to the image to show
+  */
+  imageAltText: PropTypes.string,
+  /**
+   Alt text for the image
+  */
+  imageShape: PropTypes.string,
+  /**
+   What shape the image will be - default is square
+  */
+  disabled: PropTypes.bool,
+  /**
+   If `true`, the button is disabled
+  */
+  corners: PropTypes.string,
+  /**
+   'rounded'
+  */
+  shadow: PropTypes.string,
+  /**
+   'shadow'
+  */
+  buttonType: PropTypes.string,
+  /**
+   'success' | 'error' | 'basic' | 'black' | 'white'
+  */
+  clicked: PropTypes.bool,
+  /**
+   If `true`, gets an event to fire when the button is clicked
+  */
+  buttonText: PropTypes.string,
+  /**
+   * What text to display on the button
+   */
+};
 
 export default Card;
--- src/Components/Molecules/Card/Card.stories.js
+++ src/Components/Molecules/Card/Card.stories.js
@@ -2,7 +2,73 @@ import React from 'react';
 import Card from './Card';
 
 export default {
+  component: Card,
   title: 'Molecules / Card',
 };
 
-export const Baisc = () => <Card />;
+const cardDefaults = {
+  size: 'large',
+};
+
+export const BlankDefault = () => <Card />;
+export const BlankSmall = () => <Card size={'small'} />;
+export const BlankMedium = () => <Card size={'medium'} />;
+export const Text = () => (
+  <Card {...cardDefaults} isText={true} cardText={'Card Text'} />
+);
+export const Image = () => (
+  <Card
+    {...cardDefaults}
+    isImage={true}
+    imageLink={'https://via.placeholder.com/150'}
+    altText={'placeholder alt text'}
+    imageShape={'rounded'}
+  />
+);
+export const Button = () => (
+  <Card {...cardDefaults} isButton={true} buttonText={'Card Button'} />
+);
+export const ImageButton = () => (
+  <Card
+    {...cardDefaults}
+    isImage={true}
+    isButton={true}
+    buttonText={'Card Button'}
+    imageLink={'https://via.placeholder.com/150'}
+    altText={'placeholder alt text'}
+    imageShape={'rounded'}
+  />
+);
+export const TextButton = () => (
+  <Card
+    {...cardDefaults}
+    isText={true}
+    isButton={true}
+    cardText={'Card Text'}
+    buttonText={'Card Button'}
+  />
+);
+export const TextImageButton = () => (
+  <Card
+    {...cardDefaults}
+    isImage={true}
+    isText={true}
+    isButton={true}
+    cardText={'Card Text'}
+    buttonText={'Card Button'}
+    imageLink={'https://via.placeholder.com/150'}
+    altText={'placeholder alt text'}
+    imageShape={'rounded'}
+  />
+);
+export const TextImage = () => (
+  <Card
+    {...cardDefaults}
+    isImage={true}
+    isText={true}
+    cardText={'Card Text'}
+    imageLink={'https://via.placeholder.com/150'}
+    altText={'placeholder alt text'}
+    imageShape={'rounded'}
+  />
+);
