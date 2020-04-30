a073569b00567d8b167a780d3aef5b6ba223d014
benakehurst <benakehurst@hotmail.com>
2020-04-30 09:46:28 +0100

    feat(sidemenu): wip scafolding

--- src/Components/Organisms/SideMenu/SideMenu.js
+++ src/Components/Organisms/SideMenu/SideMenu.js
@@ -9,15 +9,39 @@ import Link from '../../Atoms/Link/Link';
 import Avatar from '../../Molecules/Avatar/Avatar';
 
 class SideMenu extends Component {
+  generateLinks = () => {
+    this.props.links.map((link) => {
+      return <Link key={link.link} />;
+    });
+  };
+
   render() {
     return (
       <Aux>
-        <div className={classes.SideMenu}>Side Menu</div>
+        <div className={classes.SideMenu}>
+          {this.props.isTopSection ? (
+            <div className={classes.TopSection}>
+              <Image />
+            </div>
+          ) : null}
+          {this.props.isMidSection ? (
+            <div className={classes.MidSection}>{this.generateLinks}</div>
+          ) : null}
+          {this.props.isBottomSection ? (
+            <div className={classes.BottomSection}>
+              <Avatar />
+            </div>
+          ) : null}
+        </div>
       </Aux>
     );
   }
 }
 
-SideMenu.propTypes = {};
+SideMenu.propTypes = {
+  isTopSection: PropTypes.bool,
+  isMidSection: PropTypes.bool,
+  isBottomSection: PropTypes.bool,
+};
 
 export default SideMenu;
